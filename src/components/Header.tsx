96fa229cf89ef4c71448b595fa642e9941af280a
Pedro Gazola <ph-gazola@hotmail.com>
2022-11-13 23:03:13 -0300

    Public

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import icon from "../assets/githubicon.png";
 
-
 export default function Header() {
+
+    // const icon = 'https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg'
     return (
         <header className="bg-header h-16 w-full flex items-center justify-between ">
             <Link href={'/'} className="ml-8"  >
                 <Image alt='icone github'
+                    
                     color="white"
-                    src={icon}
+                    src= {icon}
                     width={40}
                     height={40}
                     className="hover:cursor-pointer" />
